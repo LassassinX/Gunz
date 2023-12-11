df8cf729ea1f078f158623f3d88e20d75f9cfebd
Sanjid Islam Chowdhury <56227702+LassassinX@users.noreply.github.com>
2023-12-12 03:03:32 +0600

    added audio preload

---
+++ Preload.tsx
@@ -0,0 +1,16 @@
+"use client"
+import * as sounds from "./game/gameUtils/sounds"
+
+export default () => {
+	return <>
+		{
+			Object.values(sounds).map((sound, i) => {
+				if (typeof sound === 'string')
+					return 	<audio preload="true" autoPlay={true} muted={true} src={sound} key={i}></audio>
+				else
+					return null
+			})
+		}
+	</> 
+
+}
